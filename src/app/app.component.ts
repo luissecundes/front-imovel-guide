8fd8f0b27ccf1bc4affce57f0a1f5406d0b455e3
Luis Henrique <luissecundes@gmail.com>
2025-01-21 22:44:50 -0300

    add

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 // src/app/app.component.ts
 import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router'; 
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-root',
--- src/app/user/components/users/user-routing.module.ts
+++ src/app/user/components/users/user-routing.module.ts
@@ -1,8 +1,8 @@
 // src/app/user/user-routing.module.ts
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { UserListComponent } from './components/user-list/user-list.component';
-import { UserFormComponent } from './components/user-form/user-form.component';
+import { UserListComponent } from './user-list/user-list.component';
+import { UserFormComponent } from './user-form/user-form.component';
 
 const routes: Routes = [
   { path: 'users', component: UserListComponent },
