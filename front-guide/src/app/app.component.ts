d6839d80fe049d3280f03677e01cc0c3c786cea6
Luis Henrique <luissecundes@gmail.com>
2025-01-21 23:51:52 -0300

    add

--- front-guide/src/app/app.component.ts
+++ front-guide/src/app/app.component.ts
@@ -1,12 +1,36 @@
+// src/app/app.component.ts
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    HttpClientModule,
+    RouterModule,
+    FormsModule,
+    CommonModule
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'front-guide';
+  // Lista de usuários para armazenar os dados inseridos
+  users: any[] = [];
+
+  // Método para adicionar os dados do formulário à tabela
+  addUser(nome: string, cpf: string, creci: string): void {
+    const newUser = { nome, cpf, creci };
+    this.users.push(newUser);
+  }
+
+  // Método que é chamado quando o formulário é enviado
+  onSubmit(form: any): void {
+    if (form.valid) {
+      this.addUser(form.value.nome, form.value.cpf, form.value.creci);
+      form.reset(); // Limpar o formulário após o envio
+    }
+  }
 }
--- front-guide/src/app/app.module.ts
+++ front-guide/src/app/app.module.ts
@@ -1,15 +1,12 @@
 // src/app/app.module.ts
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
-import { RouterModule } from '@angular/router';
-import { bootstrapApplication } from '@angular/platform-browser';
-import { UserModule } from './components/users/user.module';
 
 @NgModule({
-  imports: [BrowserModule, HttpClientModule, UserModule, RouterModule],
+  declarations: [],
+  imports: [BrowserModule], // Apenas no AppModule
+  providers: [],
+  bootstrap: [],
 })
 export class AppModule {}
-
-bootstrapApplication(AppComponent);
